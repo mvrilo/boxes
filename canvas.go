1e70bbb16b169a27a784967b80e6994f64cfb407
Murilo Santana <mvrilo@gmail.com>
2020-12-05 10:26:54 -0300

    fixes

--- canvas.go
+++ canvas.go
@@ -27,25 +27,37 @@ func (c *Canvas) Render() (data []byte) {
 func (c *Canvas) HorizontalRender() (final []byte) {
 	for i, box := range c.Boxes {
 		data := box.Render()
+
 		if i == 0 {
 			final = append(final, data...)
 			continue
 		}
 
-		lines := bytes.Split(final, []byte("\n"))
-		nextLines := bytes.Split(data, []byte("\n"))
-		count := len(lines[0])
+		finalLines := bytes.Split(final, []byte("\n"))
+		boxLines := bytes.Split(data, []byte("\n"))
+		width := len(finalLines[0])
 
-		for i, line := range nextLines {
-			if i >= len(lines) {
-				lines = append(lines, bytes.Repeat([]byte(" "), count))
-			}
-
-			lines[i] = append(lines[i], ' ')
-			lines[i] = append(lines[i], line...)
+		rows := len(finalLines)
+		if len(boxLines) > rows {
+			rows = len(boxLines)
 		}
 
-		final = bytes.Join(lines, []byte("\n"))
+		for j := 0; j <= rows; j++ {
+			if j >= len(finalLines) {
+				line := bytes.Repeat([]byte(" "), width)
+				finalLines = append(finalLines, line)
+			}
+
+			if j >= len(boxLines) {
+				line := bytes.Repeat([]byte(" "), len(boxLines[j-1]))
+				boxLines = append(boxLines, line)
+			}
+
+			finalLines[j] = append(finalLines[j], ' ')
+			finalLines[j] = append(finalLines[j], boxLines[j]...)
+		}
+
+		final = bytes.Join(finalLines, []byte("\n"))
 	}
 
 	return
--- examples/canvas/main.go
+++ examples/canvas/main.go
@@ -5,10 +5,13 @@ import "github.com/mvrilo/boxes"
 func main() {
 	canvas := boxes.NewCanvas()
 
-	box, _ := boxes.New().Padding(3).WriteString("hello world")
+	box, _ := boxes.New().Padding(3).WriteString("boxes is a simple\nascii box builder")
 	canvas.AddBox(box)
 
-	box, _ = boxes.New().Padding(3).WriteString("boxes is a simple\nascii box builder\n\n:)")
+	box, _ = boxes.New().Padding(3).WriteString(":)")
+	canvas.AddBox(box)
+
+	box, _ = boxes.New().Padding(3).WriteString("1\n2\n3\n4\n5")
 	canvas.AddBox(box)
 
 	println(string(canvas.HorizontalRender()))
